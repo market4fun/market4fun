238f07dbfa3e6762cb36a53985855ce2a70643c7
Bruno Botelho <brunorabelow@hotmail.com>
2019-05-29 00:15:47 -0300

    Alteração da página inicial

--- market4fun/urls.py
+++ market4fun/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
    path('users/',include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', include('ibroker.urls'), name='ibroker'), #Homepage é a homepage do app ibroker
+    path('ibroker', include('ibroker.urls'), name='ibroker'), #Homepage é a homepage do app ibroker
 
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='base/index.html'), name='home'),
 
 ]
