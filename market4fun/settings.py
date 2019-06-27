a3674811e6a24f019de1a7ae9c25181a841ba7f0
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 01:28:49 -0300

    heroku

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -86,15 +86,13 @@ LOGOUT_REDIRECT_URL = 'ibroker:home'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -114,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
@@ -148,5 +147,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-# This is new
-del DATABASES['default']['OPTIONS']['sslmode']
