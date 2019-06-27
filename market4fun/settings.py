ba5a5fa9094b3414e86f81f832573932399db60c
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 00:48:11 -0300

    static fix

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
 STATIC_URL = '/static/'
 
