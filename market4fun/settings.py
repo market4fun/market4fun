d714eda9c4afa3225d6c6096d43599096f71c861
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 02:10:36 -0300

    database test

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -86,17 +86,11 @@ LOGOUT_REDIRECT_URL = 'ibroker:home'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
