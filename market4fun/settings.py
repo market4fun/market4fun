c360dc966908e99bfcb30bdd13e901ff5e446e74
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 01:09:01 -0300

    Fix database for heroku

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -86,12 +86,13 @@ LOGOUT_REDIRECT_URL = 'ibroker:home'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
+DATABASES = {}
 
 
 # Password validation
