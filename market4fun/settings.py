6e74792bc97b772a16ed5240e0165709a041e849
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 03:22:36 -0300

    heroku

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -86,14 +86,11 @@ LOGOUT_REDIRECT_URL = 'ibroker:home'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
@@ -115,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
