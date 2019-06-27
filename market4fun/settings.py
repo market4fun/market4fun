de23042c68ef2466d65e8ce6f87a08c3a283d4da
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2019-06-27 00:58:45 -0300

    heroku

--- market4fun/settings.py
+++ market4fun/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
-STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
